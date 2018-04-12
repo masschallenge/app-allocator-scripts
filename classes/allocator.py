5e5ab1dd581dbe034a26aa5a3b7c94cfdad88e27
Nathan Wilson <nathan@masschallenge.org>
2018-04-12 13:44:52 +0000

    Move bin generation to Allocator object

--- classes/allocator.py
+++ classes/allocator.py
@@ -17,9 +17,15 @@ from classes.judge import Judge
 from classes.property import program
 from classes.property import industry
 from classes.reads_bin import ReadsBin
+from classes.role_bin import RoleBin
 from classes.satisfied_bin import SatisfiedBin
 from classes.startup import Startup
 
+FEMALE_WEIGHT = 2
+ROLE_WEIGHT = 3
+HOME_PROGRAM_WEIGHT = 4
+JUDGE_ZSCORE_WEIGHT = 5
+INDUSTRY_WEIGHT = 2
 
 
 class Allocator(object):
@@ -52,22 +58,31 @@ class Allocator(object):
             for bin in self.bins:
                 bin.add_startup(startup)
 
+    def calc_capacity(self):
+        for bin in self.bins:
+            bin.calc_capacity(self.judges)
+
     def work_left(self):
         return any([bin.work_left() for bin in self.bins])
 
     def default_bins(self):
         self.bins = (
             [ReadsBin(),
-             FemaleBin(weight=2*BIN_DEFAULT_WEIGHT),
-             SatisfiedBin()] +
+             SatisfiedBin(),
+             RoleBin(value="Executive",
+                     weight=ROLE_WEIGHT*BIN_DEFAULT_WEIGHT,
+                     count=2),
+             RoleBin(value="Lawyer",
+                     weight=ROLE_WEIGHT*BIN_DEFAULT_WEIGHT),
+             RoleBin(value="Investor",
+                     weight=ROLE_WEIGHT*BIN_DEFAULT_WEIGHT)] +
             bin_factory(IndustryBin,
                         values=[value for value, _ in industry.values],
-                        weight=4*BIN_DEFAULT_WEIGHT) +
+                        weight=INDUSTRY_WEIGHT*BIN_DEFAULT_WEIGHT) +
             bin_factory(HomeProgramBin,
                         values=[value for value, _ in program.values],
-                        weight=3*BIN_DEFAULT_WEIGHT))
+                        weight=HOME_PROGRAM_WEIGHT*BIN_DEFAULT_WEIGHT))
 
-        
     def allocate (self):
         while self.work_left() and self.judges:
             judge = choice(self.judges)
