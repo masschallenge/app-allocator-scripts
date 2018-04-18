8363398f2ad50bd3d0f8a23c21f2cbf541a2aa2e
Jon Kiparsky <jon@kiparsky.net>
2018-04-18 11:28:49 -0400

    [AC-5527] Fix name creation for judges

--- classes/entity.py
+++ classes/entity.py
@@ -44,12 +44,15 @@ class Entity(object):
             self.properties[property.name] = value
 
     def add_fields_to_name(self, fields):
-        name = self.properties["name"]
+        name = self.properties.get("name") or self._generated_name()
         for field in fields:
             name += "-{value}".format(value=self.properties[field])
         self.properties["name"] = name
 
-
+    def _generated_name(self):
+        return "%s-%s" % (self.type, self.properties['id'])
+        
+    
 def csv_output(entities):
     print(CSV_HEADER)
     for entity in entities:
