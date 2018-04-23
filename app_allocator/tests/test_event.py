3a011ecb25fd38b414cf12d82520e90b1741632e
Jon Kiparsky <jon@kiparsky.net>
2018-04-23 16:20:14 -0400

    [AC-5520] Coverage for event.py

---
+++ app_allocator/tests/test_event.py
@@ -0,0 +1,24 @@
+from app_allocator_scripts.classes.event import Event
+
+
+event_fields = {'a': 'bcdef', 'b': 'cdefghi'}
+
+class TestEvent(object):
+    def test_event_to_csv(self):
+        event = Event(**event_fields)
+        csv_fields = event.to_csv().split(",")
+        assert all([field in csv_fields for field in event_fields])
+
+    def test_update_event_existing_field(self):
+        event = Event(**event_fields)
+        value = 'tuvwxy'
+        event.update({'b': value})
+        assert event.fields['b'] == value
+
+    def test_update_event_new_field(self):
+        event = Event(**event_fields)
+        value = 'tuvwxy'
+        event.update({'c': value})
+        assert event.fields['c'] == value
+
+    
