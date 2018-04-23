f19f24e8cd9588517daf7dd9b998f140c6021e84
Jon Kiparsky <jon@kiparsky.net>
2018-04-23 18:40:54 -0400

    [AC-5520] Complete coverage for home_program and industry bins

--- app_allocator/classes/industry_bin.py
+++ app_allocator/classes/industry_bin.py
@@ -1,16 +1,17 @@
-from classes.bin import (
+from app_allocator.classes.bin import (
     BIN_DEFAULT_WEIGHT,
     Bin,
 )
 
 
 class IndustryBin(Bin):
+    name_format = "{} Industry Bin"
     def __init__(self, value, weight=BIN_DEFAULT_WEIGHT):
         super().__init__(weight)
         self.industry = value
 
     def __str__(self):
-        return "{} Industry Bin".format(self.industry)
+        return self.name_format.format(self.industry)
 
     def match(self, entity):
         return entity.properties["industry"] == self.industry
---
+++ app_allocator/tests/test_industry_bin.py
@@ -0,0 +1,26 @@
+from app_allocator.classes.industry_bin import IndustryBin
+from app_allocator.classes.startup import Startup
+
+WEIGHT = 23
+INDUSTRY = "Basket Weaving"
+OTHER_INDUSTRY = "Ceramics"
+        
+class TestIndustryBin(object):
+    def test_str(self):
+
+        bin = IndustryBin(INDUSTRY, WEIGHT)
+        assert str(bin) == IndustryBin.name_format.format(INDUSTRY)
+
+    def test_match_startup_matches_bin(self):
+        bin = IndustryBin(INDUSTRY, WEIGHT)        
+        startup = Startup()
+        startup.properties['industry'] = INDUSTRY
+        assert bin.match(startup)
+
+
+    def test_match_startup_does_not_match_bin(self):
+        bin = IndustryBin(INDUSTRY, WEIGHT)        
+        startup = Startup()
+        startup.properties['industry'] = OTHER_INDUSTRY
+        assert not bin.match(startup)
+        
