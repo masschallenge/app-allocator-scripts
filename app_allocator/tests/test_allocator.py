b2a7b3d0d74b24fa7361478f1f3e50796e133bc6
Jon Kiparsky <jon@kiparsky.net>
2018-04-24 09:43:56 -0400

    [AC-5520] add test cases for allocator and generator

---
+++ app_allocator/tests/test_allocator.py
@@ -0,0 +1,1 @@
+from app_allocator.classes.allocator import Allocator
---
+++ app_allocator/tests/test_generator.py
@@ -0,0 +1,10 @@
+from app_allocator.classes.generator import Generator
+
+class TestGenerator(object):
+    def test_generator_generates_judges(self):
+        generator = Generator(judge_count=10, startup_count=100)
+        assert len(generator.judges) == 10
+
+    def test_generator_generates_startups(self):
+        generator = Generator(judge_count=10, startup_count=100)
+        assert len(generator.startups) == 100        
