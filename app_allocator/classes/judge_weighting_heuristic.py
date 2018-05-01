b126564d58d8475f187f5389fb8c019afbb4cb62
Jon Kiparsky <jon@kiparsky.net>
2018-05-01 15:03:39 -0400

    [AC-5525] implement find_one_application

--- app_allocator/classes/judge_weighting_heuristic.py
+++ app_allocator/classes/judge_weighting_heuristic.py
@@ -18,34 +18,49 @@ class JudgeWeightingHeuristic(object):
     expected_reads = 4
     
     def setup(self, judges, startups):
-        self.judges = judges
-        self.startups = startups
+        self.judges = tuple(judges)
+        self.startups = tuple(startups)
         self.feature_value_set = self._feature_values(judges)
         self.feature_value_set.update(self._feature_values(startups))
         self.feature_values = list(self.feature_value_set)
         self.feature_weights = [(feature.field, 1) for feature in self.features]
         self.judge_matrix = matrix(self._to_array(judges, judge_row_value))
         self.startup_needs = self._startup_needs()
+        self.judge_startup_weights = self._calc_weightings()
         
     def work_left(self):
-        return False
+        return True
     
     def find_one_application(self, judge):
-        return None
+        row_number = self.judges.index(judge)
+        row = self.judge_startup_weights[row_number]
+        top_app_indexes = self.find_top_app_indexes(row, judge)
+        return self.startups[choice(top_app_indexes)]
+
+    def find_top_app_indexes(self, row, judge):
+        top_score = max(row)
+        return [ix for ix, val in enumerate(row) if val == top_score]
     
     def assess(self):
         pass
-
 
     def _feature_values(self, entities):
         return set([(feature, entity[feature.field])
                     for entity in entities for feature in self.features])
 
+    def _calc_weightings(self):
+        startup_needs_matrix = matrix([list(row.values()) for row in self.startup_needs.values()])
+
+                                      
+        return array(self.judge_matrix * (startup_needs_matrix.transpose()))
+    
     def _startup_needs(self):
-        needs = []
+        needs = OrderedDict()
         for startup in self.startups:
-            needs.append([feature.initial_need(startup, value) for feature, value in self.feature_values])
-        return matrix(needs)
+            row = OrderedDict({(feature, value): feature.initial_need(startup, value)
+                               for feature, value in self.feature_values})
+            needs[startup] = row
+        return needs
 
 
     def _to_array(self, entities, row_value):
@@ -56,7 +71,11 @@ class JudgeWeightingHeuristic(object):
                 row[(feature, entity[feature.field])] = row_value(entity, feature)
             rows.append(list(row.values()))
         return array(rows)
-            
+
+    def update_startup_needs(self, judge, startup):
+        for key, val in judge.properties.items():
+            if (key, val) in self.startup_needs[startup]:
+                self.startup_needs[(key, val)] -= 1
 
 def calc_judge_startup_weights(startups):
     startups_matrix = matrix(self._to_array(startups))
