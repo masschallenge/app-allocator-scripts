22b79c282cadd650be088c0d1814d72f5861bd84
Jon Kiparsky <jon@kiparsky.net>
2018-05-31 15:53:22 -0400

    [AC-5654] Delete dead metrics

--- app_allocator/classes/industry_match_metric.py
+++
@@ -1,9 +0,0 @@
-from app_allocator.classes.metric import Metric
-
-
-class IndustryMatchMetric(Metric):
-    def condition(self, judge, application):
-        return judge['industry'] == application['industry']
-
-    def output_key(self):
-        return "Matching Industry"
