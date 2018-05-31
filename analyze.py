d031749e62e8fedce0d7115c56299dd3c58815e0
Jon Kiparsky <jon@kiparsky.net>
2018-05-31 13:12:28 -0400

    [AC-5654] modify summarize to work with new representation

--- analyze.py
+++ analyze.py
@@ -10,16 +10,15 @@ if len(argv) > 2:
     allocation = argv[2]
 else:
     allocation = "tmp.out"
+if len(argv) > 3:
+    criteria = argv[3]
+else:
+    criteria = "criteria.csv"
 
-
-aa = quick_setup(scenario, allocation)
-for summary, word in [(aa.summarize(aa.assigned, prefix="Assigned"),
-                       "Assigned"),
-                      (aa.summarize(aa.completed, prefix="Completed"),
-                       "Completed")]:
-    print("\n%s Application Stats\n------------------\n" % word)
-    print("\n".join(["%s: %s" % (key, val)
-                     for key, val in summary.items()]))
-
+aa = quick_setup(scenario, allocation, criteria)
+print ("Assigned: ")
+print (aa.summarize(aa.assigned))
+print ("Completed: ")
+print (aa.summarize(aa.completed))
 
 print()
--- app_allocator/classes/allocation_analyzer.py
+++ app_allocator/classes/allocation_analyzer.py
@@ -5,15 +5,7 @@ from collections import (
 )
 from app_allocator.classes.judge import Judge
 from app_allocator.classes.application import Application
-from app_allocator.classes.gender_distribution_metric import (
-    GenderDistributionMetric,
-)
-from app_allocator.classes.judge_role_distribution_metric import (
-    JudgeRoleDistributionMetric,
-)
-from app_allocator.classes.program_match_metric import ProgramMatchMetric
-from app_allocator.classes.industry_match_metric import IndustryMatchMetric
-from app_allocator.classes.total_reads_metric import TotalReadsMetric
+from app_allocator.classes.criteria_reader import CriteriaReader
 
 Assignment = namedtuple("Assignment", ["judge", "application"])
 TOTAL_READS_TARGET = 4
@@ -25,19 +17,11 @@ class AllocationAnalyzer(object):
         self.applications = {}
         self.assigned = []
         self.completed = []
-        self.metrics = [TotalReadsMetric(TOTAL_READS_TARGET),
-                        IndustryMatchMetric(1),
-                        ProgramMatchMetric(1),
-                        ]
-        self.metrics.extend([
-            JudgeRoleDistributionMetric('Lawyer', 1),
-            JudgeRoleDistributionMetric('Executive', 2),
-            JudgeRoleDistributionMetric('Investor', 1),
-            JudgeRoleDistributionMetric('Other', 0)])
-        self.metrics.extend([
-            GenderDistributionMetric('female', 1),
-            GenderDistributionMetric('male', 0)])
-
+        
+    def read_criteria(self, criteria_file):
+        with open(criteria_file) as file:
+            self.criteria = CriteriaReader(file).all()
+    
     def process_scenario_from_csv(self, input_file):
         reader = open_csv_reader(input_file)
         for row in reader:
@@ -62,44 +46,30 @@ class AllocationAnalyzer(object):
                 self.completed.append(Assignment(judge, application))
 
     def analyze(self, assignments):
-        # analysis = {}
-        # for criterion in self.criteria:
-        #     analysis.update(criterion.evaluate(assignments, self.applications))
-        # return analysis
-        for metric in self.metrics:
-            metric.total = 0
-        read_counts = {application['name']: defaultdict(int)
-                       for application in self.applications.values()}
-        for assignment in assignments:
-            for metric in self.metrics:
-                metric.evaluate(assignment, read_counts)
-        return read_counts
+        analysis = {}
+        for criterion in self.criteria:
+            analysis.update(criterion.evaluate(assignments, self.applications))
+        return analysis
 
     def summarize(self, assignments, prefix=""):
-        self.analyze(assignments)
-        summary = metrics_summary(self.metrics, prefix)
-        summary['total_applications'] = len(self.applications)
-        summary['total_judges'] = len(self.judges)
-        return summary
+        analysis = self.analyze(assignments)
+        output_lines = []
+        for criterion in self.criteria:
+            row = analysis[criterion.name()]
+            for option, counts in row.items():
+                counts_string = ",".join([": ".join((str(k), str(v))) for k, v in sorted(counts.items(), reverse=True)])
+                
+                output_lines.append("%s -- %s: %s" % (criterion.name(), option, counts_string))
+        return "\n".join(output_lines)
 
 
-def metrics_summary(metrics, prefix):
-    summary = defaultdict(int)
-    for metric in metrics:
-        summary['%s: total %s' % (prefix,
-                                  metric.output_key())] = metric.total
-        summary['%s: max %s (%s)' % (prefix,
-                                     metric.output_key(),
-                                     metric.max_app)] = metric.max_count
-        missed_count = len(metric.unsatisfied_apps)
-        summary['%s: missed %s' % (prefix, metric.output_key())] = missed_count
-    return summary
 
 
-def quick_setup(scenario='example.csv', allocation='tmp.out'):
+def quick_setup(scenario='example.csv', allocation='tmp.out', criteria="criteria.csv"):
     aa = AllocationAnalyzer()
     aa.process_scenario_from_csv(scenario)
     aa.process_allocations_from_csv(allocation)
+    aa.read_criteria(criteria)
     return aa
 
 
